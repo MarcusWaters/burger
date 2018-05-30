19e9112fd8162f48ce6d8311394fa85f57de69b5
Marcus Waters <marcus.waters@gmail.com>
2018-05-30 06:15:56 -0500

    heroku

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,23 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
+if (process.env.JAWSBD_URL) {
+    connection = mysql.createConnection(process.env.JAWSBD_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'burgers_db'
+    });
+};
+
+
+
+
+
+
+
 // Make connection.
 connection.connect(function(err) {
   if (err) {
